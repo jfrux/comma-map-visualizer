014a12992fae892547ae5f827cda27757b97ffe8
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-09-26 20:15:17 -0400

    Initial commit of basic react structure.

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../assets/images/comma.svg';
+
+const App = () => {
+  return (
+    <div>
+      <Logo />
+      <h2 id="heading"></h2>
+    </div>
+  );
+};
+
+export default App;
